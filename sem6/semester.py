575cf38d4465864f29f065deda1192310f52f321
Shaikh Arbaz Ahmed <139984287+arbaz5656@users.noreply.github.com>
2023-10-17 02:42:54 +0530

    sem 6

---
+++ sem6/semester.py
@@ -0,0 +1,24 @@
+# import pandas as pd
+# import numpy as np
+# section=['A','B','C','D','E',]
+# contri=np.array([2725,3600,4250,1200,np.NaN])
+# s12=pd.Series(data=contri*2,index=section,dtype=np.float32)
+# print(s12)
+
+import numpy as np
+import matplotlib.pyplot as plt
+Data=[[115, 15, 25,35],[ 19, 18,21,15],[2,18, 10, 30], [13,27, 20, 35]]
+
+X=np.arange(4)
+
+plt.plot(X, Data[0], color="b", label="Rangel")
+
+pli plotts. Datal 11 color "" label "Rangel")
+
+plt plot(x Data 2 color" Isbel "Rangel")
+
+pli plots, Data 3 color label "Rangel") plt legend(loc upper lett plt title("Multirange Line C)
+
+plt.xlabel('X')
+plt.ylabel("Y")
+plt.show()
